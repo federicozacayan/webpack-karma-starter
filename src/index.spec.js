99e9fff46acc58b94a4603f55ee4150a09dab72c
Federico Zacayan <55717297+federicozacayan@users.noreply.github.com>
2020-02-14 15:30:48 +1300

    Update index.spec.js

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,10 +1,32 @@
-import test from './';
+import test, {A} from './';
 
-describe('index', () => {
-    it('test', () => {
+describe('Testing File index.html', () => {
+    
+    it('Test else way', () => {
         expect(test(true)).toBeTruthy();
     })
-    it('throw', () => {
-        expect(() => {test('wrong');}).toThrowError(Error, 'must be boolean');
+    
+    it('Get an Exeption', () => {
+        expect(() => {test('wrong');})
+        .toThrowError(Error, 'must be boolean');
+    })
+    
+    it('Get true', ()=>{
+        let a = new A;
+        expect(a.run()).toBe(true);
+    })
+    
+    it('Get False', ()=>{
+        let a = new A;
+        expect(!a.run()).toBeFalsy();
+        
+    })
+    
+    it('How many time it was called', ()=>{
+        let a = new A;
+        var spy = spyOn(a, 'run');
+        a.run()
+        expect(spy).toHaveBeenCalledTimes(1);
+        
     })
 })
